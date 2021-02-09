a91e5c63305e48eb0a4778435bc3c4fd11f2785e
Cristian <cristi.valentin29@gmail.com>
2021-02-09 17:37:48 +0100

    refactor: 💡 formatted files

--- apps/react-app/src/app/App.js
+++ apps/react-app/src/app/App.js
@@ -1,29 +1,20 @@
 import './App.css';
-import StudentList from "./component/studentList.component";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import StudentUpdate from "./component/studentUpdate.component";
-import StudentCreate from "./component/studentCreate.component";
-import React from "react";
+import StudentList from './component/studentList.component';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import StudentUpdate from './component/studentUpdate.component';
+import StudentCreate from './component/studentCreate.component';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-        <Router>
-            <Switch>
-                <Route exact
-                       path={["/students", ""]}
-                       component={StudentList}
-                />
-                <Route exact
-                       path={"/update"}
-                       component={StudentUpdate}
-                />
-                <Route exact
-                       path={"/create"}
-                       component={StudentCreate}
-                />
-            </Switch>
-        </Router>
+      <Router>
+        <Switch>
+          <Route exact path={['/students', '']} component={StudentList} />
+          <Route exact path={'/update'} component={StudentUpdate} />
+          <Route exact path={'/create'} component={StudentCreate} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- apps/react-app/src/app/App.test.js
+++ apps/react-app/src/app/App.test.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText("Employee List");
-  expect(linkElement.textContent).toBe("Employee List");
+  const linkElement = screen.getByText('Employee List');
+  expect(linkElement.textContent).toBe('Employee List');
 });
